8164127fd5a0688b898153b3b7d2f7e24591b503
laijunbin <xyz607xx@gmail.com>
2021-01-19 23:54:30 +0800

    Add wc settings tab for pb payment controls

--- controller/PBPaymentAdminController.php
+++ controller/PBPaymentAdminController.php
@@ -7,7 +7,18 @@ class PBPaymentAdminController {
 	
 	public function init() {
 		$this->registerSettingLinkAfterActivated();
+        $this->registerWooCommerceSettingsTab();
 	}
+
+    function registerWooCommerceSettingsTab() {
+        add_filter('woocommerce_settings_tabs_array', array($this, 'add_settings_tab'), 21);
+    }
+
+    function add_settings_tab($settings_tabs) {
+        // 進度條金流控制 還沒有英文對照 測試所以先用中文
+        $settings_tabs['pb_payment'] = __('進度條金流控制', 'pb_ecpay_woo');
+        return $settings_tabs;
+    }
 
 	function registerSettingLinkAfterActivated(){
 		$plugin = PB_ECPAY_PLUGIN;
